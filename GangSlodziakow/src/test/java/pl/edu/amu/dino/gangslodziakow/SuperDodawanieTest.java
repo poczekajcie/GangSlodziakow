4f97e9c69a16130f870b580ed5c0bd031fe605a0
s434758 <s434758@pc22f.labs.wmi.amu.edu.pl>
2018-11-16 17:01:17 +0100

    MichaŁ_Nawrot

--- GangSlodziakow/src/test/java/pl/edu/amu/dino/gangslodziakow/SuperDodawanieTest.java
+++ GangSlodziakow/src/test/java/pl/edu/amu/dino/gangslodziakow/SuperDodawanieTest.java
@@ -5,8 +5,12 @@
  */
 package pl.edu.amu.dino.gangslodziakow;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -17,8 +21,51 @@ public class SuperDodawanieTest {
     public SuperDodawanieTest() {
     }
 
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
     @Test
     public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of superdodawanie method, of class SuperDodawanie.
+     */
+    @Test
+    public void testSuperdodawanie() {
+        System.out.println("superdodawanie");
+        String a = "1";
+        String b = "-1";
+        String expResult = "0";
+        String result = SuperDodawanie.superdodawanie(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+        public void testSuperdodawanie2() {
+        System.out.println("superdodawanie");
+        String a = "1";
+        String b = "0";
+        String expResult = "1";
+        String result = SuperDodawanie.superdodawanie(a, b);
+        assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
