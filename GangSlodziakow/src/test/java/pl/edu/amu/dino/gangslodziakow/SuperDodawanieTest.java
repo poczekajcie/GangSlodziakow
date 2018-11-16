d9caa53c01326dfd3651b55df6fc330fa660d74c
jsiwek <jsiwek@pc22j.labs.wmi.amu.edu.pl>
2018-11-16 16:21:48 +0100

    initial

---
+++ GangSlodziakow/src/test/java/pl/edu/amu/dino/gangslodziakow/SuperDodawanieTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.dino.gangslodziakow;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jsiwek
+ */
+public class SuperDodawanieTest {
+    
+    public SuperDodawanieTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
