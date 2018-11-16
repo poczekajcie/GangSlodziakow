060f8260d3c6f5f99c77ad36b53f1cab9cfd56fa
jsiwek <jsiwek@pc22j.labs.wmi.amu.edu.pl>
2018-11-16 16:23:31 +0100

    initial2

---
+++ GangSlodziakow/src/main/java/pl/edu/amu/dino/gangslodziakow/SuperDodawanie.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.dino.gangslodziakow;
+
+/**
+ *
+ * @author jsiwek
+ */
+public class SuperDodawanie {
+    public static String (String a, String b){
+        return "0";
+    }
+}
